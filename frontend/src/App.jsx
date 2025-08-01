3a772785657dd86a165fdbdf7001a0ecdcc5f1ae
naveen-166 <naveensrn1935@gmail.com>
2025-08-02 00:51:57 +0530

    ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
   import Login from './Pages/Login'
   import { BrowserRouter, Route, Routes } from 'react-router-dom'
   import HomePage from './Pages/Student/Student.Home'
-  import StudentProfile from './Pages/Student/Student.Profile'
   import AdminHome from './Pages/Admin/Admin.Home'
-  import StudentCompanyProfile from './Pages/Student/Student.CompanyProfile'
   import { AuthProvider } from './Context/AuthContext'
 
   function App() {
@@ -13,9 +11,7 @@
         <Routes>
           <Route path='/' element={<Login/>}/>
           <Route path='/student/home' element={<HomePage/>}/>
-          <Route path='/student/profile' element={<StudentProfile/>}/>
           <Route path='/admin/home' element={<AdminHome/>}/>
-          {/* <Route path="/temp" element={<StudentCompanyProfile/>}/> */}
         </Routes>
       </BrowserRouter>
       </AuthProvider>
